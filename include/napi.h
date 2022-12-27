e518e3ed54e1c2c72d2fa5a3f06fc848b498e8bd
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-12-27 12:43:49 +0100

    Rename macro

--- include/napi.h
+++ include/napi.h
@@ -8,9 +8,9 @@ extern "C" {
 #include <js.h>
 
 #ifdef NAPI_INLINE
-#define NAPI_EXPORT static inline
+#define NAPI_INLINABLE static inline
 #else
-#define NAPI_EXPORT
+#define NAPI_INLINABLE
 #endif
 
 typedef js_env_t *napi_env;
@@ -48,145 +48,145 @@ typedef enum {
   napi_no_external_buffers_allowed
 } napi_status;
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_open_handle_scope (napi_env env, napi_handle_scope *result) {
   js_open_handle_scope(env, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_close_handle_scope (napi_env env, napi_handle_scope scope) {
   js_close_handle_scope(env, scope);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_open_escapable_handle_scope (napi_env env, napi_escapable_handle_scope *result) {
   js_open_escapable_handle_scope(env, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_close_escapable_handle_scope (napi_env env, napi_escapable_handle_scope scope) {
   js_close_escapable_handle_scope(env, scope);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_escape_handle (napi_env env, napi_escapable_handle_scope scope, napi_value escapee, napi_value *result) {
   js_escape_handle(env, scope, escapee, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_run_script (napi_env env, napi_value source, napi_value *result) {
   js_run_script(env, source, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_create_reference (napi_env env, napi_value value, uint32_t initial_refcount, napi_ref *result) {
   js_create_reference(env, value, initial_refcount, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_delete_reference (napi_env env, napi_ref ref) {
   js_delete_reference(env, ref);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_reference_ref (napi_env env, napi_ref ref, uint32_t *result) {
   js_reference_ref(env, ref, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_reference_unref (napi_env env, napi_ref ref, uint32_t *result) {
   js_reference_unref(env, ref, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_reference_value (napi_env env, napi_ref ref, napi_value *result) {
   js_get_reference_value(env, ref, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_create_int32 (napi_env env, int32_t value, napi_value *result) {
   js_create_int32(env, value, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_create_uint32 (napi_env env, uint32_t value, napi_value *result) {
   js_create_uint32(env, value, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_create_string_utf8 (napi_env env, const char *str, size_t len, napi_value *result) {
   js_create_string_utf8(env, str, len, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_create_function (napi_env env, const char *name, size_t len, napi_callback cb, void *data, napi_value *result) {
   js_create_function(env, name, len, cb, data, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_global (napi_env env, napi_value *result) {
   js_get_global(env, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_null (napi_env env, napi_value *result) {
   js_get_null(env, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_undefined (napi_env env, napi_value *result) {
   js_get_undefined(env, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_boolean (napi_env env, bool value, napi_value *result) {
   js_get_boolean(env, value, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_value_int32 (napi_env env, napi_value value, int32_t *result) {
   js_get_value_int32(env, value, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_value_uint32 (napi_env env, napi_value value, uint32_t *result) {
   js_get_value_uint32(env, value, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_get_value_string_utf8 (napi_env env, napi_value value, char *str, size_t len, size_t *result) {
   js_get_value_string_utf8(env, value, str, len, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_call_function (napi_env env, napi_value recv, napi_value fn, size_t argc, const napi_value argv[], napi_value *result) {
   js_call_function(env, recv, fn, argc, argv, result);
   return napi_ok;
 }
 
-NAPI_EXPORT napi_status
+NAPI_INLINABLE napi_status
 napi_throw (napi_env env, napi_value error) {
   js_throw(env, error);
   return napi_ok;
