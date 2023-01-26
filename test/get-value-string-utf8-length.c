7934425037aa24ac0dfa6a7fc2a2e258b6c9d32f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-01-26 13:47:02 +0100

    Test string length getter

---
+++ test/get-value-string-utf8-length.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+
+#include "../include/napi.h"
+#include "helpers.h"
+
+int
+main () {
+  int e;
+
+  napi_env env = napi_setup_env();
+
+  napi_value string;
+  e = napi_create_string_utf8(env, "hello", -1, &string);
+  assert(e == 0);
+
+  size_t len;
+  e = napi_get_value_string_utf8(env, string, NULL, 0, &len);
+  assert(e == 0);
+
+  assert(len == 5);
+
+  napi_teardown_env();
+}
